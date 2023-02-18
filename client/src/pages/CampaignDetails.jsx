d5c9a4c7e3ad7bcc8de2850eb67af12721bb8bbf
squidrye <mihirs167@gmail.com>
2023-02-18 19:15:48 +0530

    details page

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import Base from '../components/Base'
+import { Card, Typography } from '@material-tailwind/react'
 const CampaignDetails = () => {
   return (
     <Base>
-      
+      <div className="grid grid-cols-3 mt-5 gap-5 container mx-auto">
+        <img
+          src="/src/assets/courage.jpg"
+          alt="campaign--img"
+          className="w-full h-[20rem] rounded-md col-span-2"
+        />
+        <div className='grid grid-rows-3 gap-3'>
+          <Card className='h-auto w-auto mx-12'>
+            OTher value
+          </Card>
+          <Card className='h-auto w-auto mx-12'>
+            OTher value
+          </Card>
+          <Card className='h-auto w-auto mx-12'>
+            OTher value
+          </Card>
+        </div>
+      </div>
+      <div className="grid grid-cols-3 mt-5 gap-3 container mx-auto">
+        <div className='grid gird-rows-3 gap-3 '>
+          <Typography
+            as="a"
+            href="#"
+            variant="h4"
+            color="white"
+            className="mr-4 cursor-pointer py-1.5 font-bold"
+          >
+            <span>Creator</span>
+          </Typography>
+        </div>
+      </div>
     </Base>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,16 +2,20 @@ import { CampaignCard } from "../components/index.js";
 import React from "react";
 import Base from "../components/Base";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import CampaignDetails from "./CampaignDetails.jsx";
 
 const Home = () => {
   const [campaigns, setCampaigns] = useState([]);
 
-  //get all campaigns
+  //map over the campaigns and create a card for each campaign
+
   return (
     <Base>
       <div className="grid grid-cols-4 gap-3 mt-5 container mx-auto">
-        {/* map over the campaigns and create a card for each campaign */}
-        <CampaignCard />
+        <Link to="/campaign-details/1" element={< CampaignDetails />} >
+          <CampaignCard />
+        </Link>
         <CampaignCard />
         <CampaignCard />
         <CampaignCard />
